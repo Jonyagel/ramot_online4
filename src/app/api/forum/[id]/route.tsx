b5d3c6b788632ffefb6378ec1d667476453fb1f3
Jonyagel <139068520+Jonyagel@users.noreply.github.com>
2024-07-10 21:03:28 +0300

    Add files via upload

--- src/app/api/forum/[id]/route.tsx
+++ src/app/api/forum/[id]/route.tsx
@@ -9,13 +9,13 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET(req: any, route: any) {
     try {
         await connectDb();
-        // const id = req.url.split("http://localhost:3000/api/forum/")[1];
-        const {id} = req.query;
+        const id = req.url.split(`${process.env.NEXT_PUBLIC_API_URL}/api/forum/`)[1];
+        // const {id} = req.query;
         const data = await ForumModel.findOne({_id: id });
         return NextResponse.json(data);
     }
     catch (err) {
         console.log(err);
-        return NextResponse.json({ err, msg: "There problem try again later" }, { status: 502 })
+        return NextResponse.json({ err, msg: "There problem try again later" ,}, { status: 502 })
     }
 }
