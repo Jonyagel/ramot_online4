403296739bd65cfc46e3df85c7d27d51cf7a28df
Jonyagel <139068520+Jonyagel@users.noreply.github.com>
2024-07-10 20:58:26 +0300

    Add files via upload

---
+++ route.tsx
@@ -0,0 +1,21 @@
+import { connectDb } from "@/src/app/db/connectDb";
+import { ForumModel } from "@/src/app/models/forumModel";
+import { NextRequest, NextResponse } from "next/server";
+
+
+// export const dynamic = 'auto';
+
+
+export async function GET(req: any, route: any) {
+    try {
+        await connectDb();
+        const id = req.url.split("http://localhost:3000/api/forum/")[1];
+        // const {id} = req.query;
+        const data = await ForumModel.findOne({_id: id });
+        return NextResponse.json(data);
+    }
+    catch (err) {
+        console.log(err);
+        return NextResponse.json({ err, msg: "There problem try again later" ,}, { status: 502 })
+    }
+}
